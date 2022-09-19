c414b5b92c85cd89dc7dd55ab0b18d40d6b37aee
ejercicios-python <bran.abregoluis@gmail.com>
2022-09-18 19:24:35 -0600

    Actua

---
+++ patterns_6_adapter_ejercicio/src/org/formacion/MetodoAdapter.java
@@ -0,0 +1,12 @@
+package org.formacion;
+
+public class MetodoAdapter implements InternationalMoneyOrganization {
+	@Override
+	public void transfer(int cantidad, String cliente) {
+		
+	}
+	@Override
+	public int state(String cliente) {
+		return 0;
+	}
+}
---
+++ patterns_6_adapter_ejercicio/test/org/formacion/InternationMoneyTests.java
@@ -0,0 +1,30 @@
+package org.formacion;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InternationMoneyTests {
+
+	private InternationalMoneyOrganization servicio;
+	
+	@Before
+	public void init() {
+		// cambia este null por una referencia al adapter que has creado
+		servicio = new MetodoAdapter();
+	}
+	
+	@Test
+	public void test_cliente_no_existe() {
+		Assert.assertEquals(0,servicio.state("No_existo"));
+	}
+
+	@Test
+	public void test_operaciones_con_nuevo_cliente() {
+		String cliente = "nuevo";
+		servicio.transfer(100, cliente);
+		System.out.println(100 + " " + cliente);
+		servicio.transfer(-70, cliente);
+		System.out.println(30 + " " + cliente);
+	}
+}
